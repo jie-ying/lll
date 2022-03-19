1fab14420ae2fe6477a3de1e86d781f7060a5d6d
jie-ying <99215630+jie-ying@users.noreply.github.com>
2022-03-19 12:22:33 -0500

    update

--- test/src/app/login/login.component.ts
+++ test/src/app/login/login.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit, OnDestroy {
+
+  clean: any = [];
 
   constructor() { }
 
+  
+
   ngOnInit(): void {
+    setInterval(() => {})//this is data string, it can trigger multiple time
+    //this is infinity loop
+  }
+
+
+  ngOnDestroy(): void {
+    this.clean.array.forEach((element: any) => {
+      clearInterval(element)//stop the loop, data stream
+    });
+  }
+  onclickbtn(){
+    this.clean.push(setInterval(() => {}));
+    
+    
   }
 
 }
