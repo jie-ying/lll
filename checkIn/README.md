a72e73ed08e73b59e08743d1b37509f8c17beb85
jie-ying <99215630+jie-ying@users.noreply.github.com>
2022-05-02 13:16:18 -0500

    init

--- checkIn/README.md
+++ checkIn/README.md
@@ -1,4 +1,4 @@
-# Pokemon
+# CheckIn
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 13.2.5.
 
--- checkIn/karma.conf.js
+++ checkIn/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/pokemon'),
+      dir: require('path').join(__dirname, './coverage/check-in'),
       subdir: '.',
       reporters: [
         { type: 'html' },
