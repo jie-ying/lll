3e91f89160acda5bb23a8bd884465c8a7021a1f1
jie-ying <99215630+jie-ying@users.noreply.github.com>
2022-05-12 21:09:12 -0500

    landing_page

--- checkIn/src/app/statements/checkin.action.ts
+++
@@ -1,7 +0,0 @@
-import { createAction, props } from '@ngrx/store';
-import { Info, Details } from '../interface/info.model';
-
-
-export const checkin = createAction('[Check Page] checkin', props <Info>());
-export const patientsDetail = createAction('[Info Page] detail', props<Details>());
-export const checkout = createAction('checkout');
