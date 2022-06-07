da982216b7d1d9b51d497b2a471641a974515630
jie-ying <99215630+jie-ying@users.noreply.github.com>
2022-06-07 17:23:54 -0500

    init

--- mylib/karma.conf.js
+++ mylib/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/like'),
+      dir: require('path').join(__dirname, './coverage/mylib'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- mylib/src/app/app.component.spec.ts
+++ mylib/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'like'`, () => {
+  it(`should have as title 'mylib'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('like');
+    expect(app.title).toEqual('mylib');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('like app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('mylib app is running!');
   });
 });
